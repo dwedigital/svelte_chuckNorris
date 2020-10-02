0dbbf397d014eba95d468bea56d294ba4faf59b9
dwedigital <dave@dwedigital.com>
2020-10-02 08:50:51 +0000

    serveless test

--- api/test.js
+++ api/test.js
@@ -1,4 +1,9 @@
 module.exports = (req, res) => {
+    console.log.json({
+        body: req.body,
+        query: req.query,
+        cookies: req.cookies,
+      })
     res.json({
       body: req.body,
       query: req.query,
