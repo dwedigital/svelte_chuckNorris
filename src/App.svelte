b584c1efbdab3d31ca5b73409dfff253c7c1246b
Dave Edwards <dave@dwedigital.com>
2020-10-02 08:52:57 +0100

    initial committ

---
+++ src/App.svelte
@@ -0,0 +1,80 @@
+<script>
+	import {onMount} from "svelte"
+	import Button from "./components/button.svelte"
+
+	const pageTitle="The 'ole Chuck Norris Joke Generator"
+	let joke = ""
+	
+	async function getJoke() {
+				await fetch("https://api.chucknorris.io/jokes/random")
+		.then(res=>res.json())
+		.then(data=>joke = data.value)
+		.catch(err=>console.log(new Error(err.message)))
+	}
+	onMount(()=>{
+		getJoke()
+	})
+</script>
+
+<svelte:head>
+<link rel="icon" 
+      type="image/png" 
+      href="images/dwe_icon.png">
+  <title>{pageTitle}</title>
+</svelte:head>
+
+
+<main>
+	<h1>Shitty Joke Generator!</h1>
+	<hr/>
+
+	<p>
+		{joke}
+	</p>
+
+
+	<Button on:click={getJoke}/>
+</main>
+
+<style>
+	* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+hr{
+	width:20%;
+	border:1px #ff3e00 solid;
+	margin-bottom: 10px;
+}
+	
+	main {
+		
+		display:flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		text-align: center;
+		padding: 1em;
+		max-width:50vw;
+		margin: 0 auto;
+		height:100vh;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size:calc(50px + 6 * ((100vw - 680px) / 680));
+		font-weight: 100;
+		text-align: center;
+		margin-bottom: 10px;
+	}
+
+	p{
+		font-size:calc(30px + 6 * ((100vw - 680px) / 680));
+		font-weight: 100;
+	}
+
+	
+</style>
---
+++ src/components/button.svelte
@@ -0,0 +1,21 @@
+<button on:click>
+    Hit Me
+</button>
+
+<style>
+    button{
+        padding:10px 0px;
+        margin-top: 20px;
+        background-color: white;
+        border-radius: 5px;
+        border-color: rgb(165, 165, 165);
+        width:50%;
+        text-transform: uppercase;
+        font-weight:100;
+    }
+    button:hover{
+        background-color:rgb(252, 252, 252);
+        cursor: pointer;
+        font-weight: 400;
+    }
+</style>
