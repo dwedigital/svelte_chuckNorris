183e38f9daa595e33987b8c41b6b12d863b0b05d
dwedigital <dave@dwedigital.com>
2020-10-02 09:06:37 +0000

    fixed api

--- api/test.js
+++ api/test.js
@@ -1,9 +1,4 @@
 module.exports = (req, res) => {
-    console.log.json({
-        body: req.body,
-        query: req.query,
-        cookies: req.cookies,
-      })
     res.json({
       body: req.body,
       query: req.query,
